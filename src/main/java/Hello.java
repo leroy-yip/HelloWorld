fe9380b1a64abe3af882a39beeed2b6e0550d2e1
demo2 <lyip@le.com>
2016-08-07 17:24:34 -0700

    test2

---
+++ src/main/java/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * Created by demo2 on 8/2/16.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+    }
+}
